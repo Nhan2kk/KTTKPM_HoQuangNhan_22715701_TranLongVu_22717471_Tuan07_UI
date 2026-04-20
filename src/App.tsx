38f02a6825b83c00daf4762d9a63186f7e69fa91
Hồ Quang Nhân <hqn19072004@gmail.com>
2026-04-20 18:28:22 +0700

    feat: create project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+    return (
+        <>
+            <section id="center" className="bg-red-400 h-100"></section>
+        </>
+    );
+}
+
+export default App;
